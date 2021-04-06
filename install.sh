b1e04373ea2450705b40e2cbc8d1f56ce442f68e
paqman20 <80555986+paqman20@users.noreply.github.com>
2021-04-06 19:11:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,4 @@ sudo pacman -S gimp
 sudo pacman -S VLC
 sudo pacman -S gparted 
 sudo pacman -S thunderbird 
- git clone https://aur.archlinux.org/snapd.git
- cd snapd
- makepkg -si
- sudo systemctl enable --now snapd.socket
- sudo ln -s /var/lib/snapd/snap /snap
- sudo snap install snap-store
+ 
