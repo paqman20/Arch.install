bec1e53428ec149b680887e8a5167db60ee27065
paqman20 <80555986+paqman20@users.noreply.github.com>
2021-06-13 01:23:40 +0000

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,1 +1,15 @@
 
+sudo echo "alias redfurd='echo 'SHeeesh''" >> ~/.bashrc
+sudo echo "alias lla='ls -l -a'" >> ~/.bashrc 
+sudo echo "alias clr=clr" >> ~/.bashrc 
+sudo echo "alias ll='ls -l'" >> ~/.bashrc
+sudo echo "                       " >> ~/.bashrc 
+sudo echo "#surfsharkvpn aliases" >> ~/.bashrc 
+sudo echo "alias ssa='sudo surfshark-vpn attack'" >> ~/.bashrc
+sudo echo "alias ss='sudo surfshark-vpn '" >> ~/.bashrc
+sudo echo "alias sss='sudo surfshark-vpn status'" >> ~/.bashrc
+sudo echo "alias ssd='sudo surfshark-vpn down'" >> ~/.bashrc
+
+
+
+source ~/.bashrc 
