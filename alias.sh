d081482afcd893e2e5234d120c8a854a7d7e9a95
paqman20 <80555986+paqman20@users.noreply.github.com>
2021-06-13 01:22:56 +0000

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,7 +1,1 @@
 
-sudo echo "alias redfurd='echo 'SHeeesh''" >> ~/.bashrc
-sudo echo "alias lla='ls -l -a'" >> ~/.bashrc 
-sudo echo "alias clr=clear" >> ~/.bashrc 
-sudo echo "alias ll='ls -l'" >> ~/.bashrc 
-source ~/.bashrc 
-
